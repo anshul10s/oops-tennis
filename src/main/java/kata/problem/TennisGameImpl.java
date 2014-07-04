a2a3a93d01d47b67c7b0bd7a11fff6a2b0c07108
Anshul Gupta <anshul.gupta@flipkart.com>
2014-07-04 19:46:17 +0530

    removing level 2 sol

--- src/main/java/kata/problem/TennisGameImpl.java
+++ src/main/java/kata/problem/TennisGameImpl.java
@@ -2,43 +2,12 @@ package kata.problem;
 
 public class TennisGameImpl implements TennisGame {
 
-    private int score1;
-    private int score2;
-
-    private String player1;
-    private String player2;
-
-    public TennisGameImpl(String player1, String player2) {
-        super();
-        this.player1 = player1;
-        this.player2 = player2;
-    }
-
     public void wonPoint(String playerName) {
-        if (player1.equals(playerName)) {
-            score1++;
-        } else if (player2.equals(playerName)) {
-            score2++;
-        }
+
     }
 
     public String getScore() {
-        return scoreDisplayValue(score1)+"-"+scoreDisplayValue(score2);
-    }
-    
-    public String scoreDisplayValue(int score) {
-        switch (score) {
-        case 0:
-            return "Love";
-        case 1:
-            return "Fifteen";
-        case 2:
-            return "Thirty";
-        case 3:
-            return "Forty";
-        default:
-            return null;
-        }
+        return null;
     }
 
 }
